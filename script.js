c7909d2da002f5b1c002ed364cf8e89aad4df4b4
Samuel Clitty <samclitty@gmail.com>
2020-09-18 21:10:25 -0700

    updated script

--- script.js
+++ script.js
@@ -4,14 +4,21 @@ console.log(moment().format("H"));
 console.log(moment().format("h A"));
 
 // Creating array for work day hours //
-var timeArray = ["9 AM", "10 AM", "11 AM", "12PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+var timeArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+// Creating an array to save to local storage 
+// var textSave = [];
+
+// Variable to get current time 
+var currentTime = moment().format("H");
+console.log(currentTime);
 
 // DOM Elements
 var currentDay = $("#currentDay");
 var container = $(".container");
 
-// Dynamically creating current date/time for page header 
 
+// Dynamically creating current date/time for page header 
 currentDay.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
 for (var i = 0; i < timeArray.length; i++) {
@@ -29,42 +36,39 @@ for (var i = 0; i < timeArray.length; i++) {
     timeRow.append(saveCol);
     saveCol.append(btnIcon);
 
-    // Adding 
-    hourCol.text(timeArray[i]); 
-    textCol.attr("id", "text" + i);
+    // Adding separate id and data 
+    textCol.attr("data-time", i);
+    textCol.attr("id", i);
 
 
-    
+    // Adding content to page elements
+    // Change from 24hr clock
+    var timeChange = timeArray[i] - 12;
+    if (timeArray[i] < 12) {
+        hourCol.html(`${timeArray[i]} AM`);
+        } else if (timeArray[i] === 12) {
+        hourCol.html(`12 PM`)
+        } else {
+        hourCol.html(`${timeChange} PM`);
+        };
 
-
-
+    // Setting colors based on current time 
+    if (timeArray[i] < currentTime) {
+        textCol.attr("class", "col-md-8 past");
+    } else if (timeArray[i] > currentTime) {
+        textCol.attr("class", "col-md-8 future");
+    } else if (timeArray == currentTime) {
+        textCol.attr("class", "col-md-8 present");
+    }
 
 };
 
 
 
 
-// TODO: create one row with time, text area and button using JQ
-
-// TODO: Ref layout to make using JQ
-
-{/* <div class="row time-block">
-<div class="col-md-2 hour">
-  9 am
-</div>
-<textarea class="col-md-8">
-  
-</textarea>
-<button class="col-md-2 saveBtn">
-  SAVE
-</button>
-</div> */}
 
 
 
-// TODO: make 'for' loop to create the layouts for each hour. give content and appropriate classes.
-
-// ref snip ----------
 
 // TODO: Create event listener for buttons. 
 
